a24705832d417fd4c6d8951c80fe69ec8575264b
eumtsspfc <mateus.ramos.martins@escola.pr.gov.br>
2025-06-12 17:25:51 +0000

    Adicionando Java Script

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'São Paulo',
+    'Qual é a origem das cores do uniforme do São Paulo?',
+    'As cores vermelho, branco e preto representam a fusão dos clubes Club Athletico Paulistano e Associação Atlética das Palmeirasd, que deram origem ao São Paulo FC em 1930.'
+)
+
+criaCartao(
+    'São Paulo',
+    'Qual é o maior título internacional do São Paulo?',
+    'A Copa do Mundo de Clubes da FIFA de 2005 (chamada de Mundial de Clubes), quando venceu o Liverpool por 1 a 0, com gol de Mineiro.'
+)
+
+criaCartao(
+    'São Paulo',
+    'Por que o mascote do São Paulo é o Santo Paulo?',
+    'O mascote é inspirado no apóstolo São Paulo, que dá nome ao clube e à cidade. Ele representa tradição e valores do time.'
+)
